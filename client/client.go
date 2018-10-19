9549fd23efa66a8b8b2eb9b8ca2d22bdb86c4f8d
zhenghaitao <hb.zhenghaitao@aisino.com>
2018-10-19 15:05:50 +0800

    initial commit

---
+++ client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	pb "grpc-tls/proto"
+	"log"
+)
+
+const (
+	Address = "127.0.0.1:50052"
+)
+
+func main() {
+	//TLS连接
+	creds, err := credentials.NewClientTLSFromFile("static/server.crt", "server name")
+	if err != nil {
+		log.Fatalf("Failed to create TLS credentials %v", err)
+	}
+
+	conn, err := grpc.Dial(Address, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewHelloClient(conn)
+	reqBody := new(pb.HelloRequest)
+	reqBody.Name = "gRPC"
+	r, err := c.SayHello(context.Background(), reqBody)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(r.Message)
+}
---
+++ server/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	pb "grpc-tls/proto"
+	"log"
+	"net"
+)
+
+const (
+	// gRPC服务地址
+	Address = "127.0.0.1:50052"
+)
+
+//定义helloService并实现约定的接口
+type helloService struct{}
+
+func (h helloService) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	resp := new(pb.HelloReply)
+	resp.Message = "Hello " + in.Name + "."
+	return resp, nil
+}
+
+var HelloService = helloService{}
+
+func main() {
+	listen, err := net.Listen("tcp", Address)
+	if err != nil {
+		log.Fatalf("failed to listen:%v", err)
+	}
+
+	//TLS认证
+	creds, err := credentials.NewServerTLSFromFile("static/server.crt", "static/server.key")
+	if err != nil {
+		log.Fatalf("failed to generate credentials %v", err)
+	}
+
+	s := grpc.NewServer(grpc.Creds(creds))  //实例化grpc Server，并开启TLS认证
+	pb.RegisterHelloServer(s, HelloService) //注册HelloService
+
+	log.Println("Listen on " + Address + " with TLS")
+	s.Serve(listen)
+}
